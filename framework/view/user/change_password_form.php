4f93101b202dac97b0def950ff03544393380d01
Foxtsumaki <foxtsumaki@hotmail.com>
2024-07-07 12:51:38 +0700

    Feat: Fully functioning change password form

--- framework/view/user/change_password_form.php
+++ framework/view/user/change_password_form.php
@@ -1,56 +1,77 @@
+<?php
+
+helper('view/alert');
+
+?>
 <div class="row">
-                <div class="col-md-6">
-                  <div class="card mb-4">
-                    <h5 class="card-header">เปลี่ยนรหัสผ่าน</h5>
-                    <div class="card-body">
-                    <div class="alert alert-danger" role="alert">รหัสผ่านปัจจุบันไม่ถูกต้อง! กรุณาตรวจสอบอีกครั้ง</div>
-                    <div class="form-floating">
-                        <input
-                          type="text"
-                          class="form-control"
-                          id="floatingInput"
-                          placeholder="John Doe"
-                          aria-describedby="floatingInputHelp"
-                        />
-                        <label for="floatingInput">รหัสผ่านปัจจุบัน</label>
-                        <div id="floatingInputHelp" class="form-text">
-                          
-                        </div>
-                      </div>
-                      <div class="form-floating">
-                        <input
-                          type="text"
-                          class="form-control"
-                          id="floatingInput"
-                          placeholder="John Doe"
-                          aria-describedby="floatingInputHelp"
-                        />
-                        <label for="floatingInput">รหัสผ่านใหม่</label>
-                        <div id="floatingInputHelp" class="form-text">
-
-                        </div>
-                      </div>
-                      <div class="form-floating">
-                        <input
-                          type="text"
-                          class="form-control"
-                          id="floatingInput"
-                          placeholder="John Doe"
-                          aria-describedby="floatingInputHelp"
-                        />
-                        <label for="floatingInput">ยืนยันรหัสผ่านใหม่</label>
-                        <div id="floatingInputHelp" class="form-text">
-
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="d-grid gap-2 col-lg-6 col-md-12 mx-auto mt-3">
-                        </div>
-                        <div class="d-grid gap-2 col-lg-6 col-md-12 mx-auto mt-3">
-                          <button class="btn btn-primary btn-lg" type="button">บันทึก</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+  <div class="col-md-6">
+    <div class="card mb-4">
+      <h5 class="card-header">เปลี่ยนรหัสผ่าน</h5>
+      <div class="card-body">
+      <?php
+        if(!empty($_SESSION['response']['alert'])){
+          gen_alert($_SESSION['response']['alert']);
+          $_SESSION['response'] = null;
+        }
+      ?>
+        <form action="<?= site_url('user/change_password') ?>" method="post">
+          <div class="form-floating">
+              <input
+                type="password"
+                name="current_password"
+                class="form-control"
+                id="floatingInput"
+                placeholder="John Doe"
+                aria-describedby="floatingInputHelp"
+                minlength=8
+                required
+              />
+              <label for="floatingInput">รหัสผ่านปัจจุบัน</label>
+              <div id="floatingInputHelp" class="form-text">
+                
               </div>
+          </div>
+          <div class="form-floating">
+              <input
+                type="password"
+                name="new_password"
+                class="form-control"
+                id="floatingInput"
+                placeholder="John Doe"
+                aria-describedby="floatingInputHelp"
+                minlength=8
+                required
+              />
+              <label for="floatingInput">รหัสผ่านใหม่</label>
+              <div id="floatingInputHelp" class="form-text">
+
+              </div>
+          </div>
+          <div class="form-floating">
+              <input
+                type="password"
+                name="confirm_password"
+                class="form-control"
+                id="floatingInput"
+                placeholder="John Doe"
+                aria-describedby="floatingInputHelp"
+                minlength=8
+                required
+              />
+              <label for="floatingInput">ยืนยันรหัสผ่านใหม่</label>
+              <div id="floatingInputHelp" class="form-text">
+
+              </div>
+          </div>
+          <div class="row">
+            <div class="d-grid gap-2 col-lg-6 col-md-12 mx-auto mt-3">
+            </div>
+            <div class="d-grid gap-2 col-lg-6 col-md-12 mx-auto mt-3">
+              <button class="btn btn-primary btn-lg" type="submit">บันทึก</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
