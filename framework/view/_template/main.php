297ef28a6697dc4d3206853f841ccf6ca5e275c1
Foxtsumaki <foxtsumaki@hotmail.com>
2024-07-08 16:08:55 +0700

    Fix: avatar on sidebar not change

--- framework/view/_template/main.php
+++ framework/view/_template/main.php
@@ -181,7 +181,7 @@ foreach($active_menu as $m){
                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
                   <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                      <img src="./images/robot-modern-style-vector.jpg" alt class="w-px-40 h-auto rounded-circle" />
+                      <img src="./writable/images/profile/<?= $_SESSION['user']['picture'] ?>" alt class="w-px-40 h-auto rounded-circle" />
                     </div>
                   </a>
                   <ul class="dropdown-menu dropdown-menu-end">
