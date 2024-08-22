a5e2567dd7a86b8e433d28994b9f3d4bf5761aba
Foxtsumaki <foxtsumaki@hotmail.com>
2024-08-23 00:23:50 +0700

    fix: duplicate function at function.php

--- framework/helper/view/form.php
+++ framework/helper/view/form.php
@@ -7,11 +7,11 @@ function gen_select($id, $name, $selection, $default_selection)
     echo('</select>');
 }
 
-function gen_option($selection, $default_selection){
-    $ret='';
-    foreach ($selection as $key => $value) {
-        $select_user_type = ($value == $default_selection) ? 'selected' : '';
-        $ret.='<option value="'.$key.'" '.$select_user_type.'>'.$value.'</option>';
-    }
-    return $ret;
-}
+// function gen_option($selection, $default_selection){
+//     $ret='';
+//     foreach ($selection as $key => $value) {
+//         $select_user_type = ($value == $default_selection) ? 'selected' : '';
+//         $ret.='<option value="'.$key.'" '.$select_user_type.'>'.$value.'</option>';
+//     }
+//     return $ret;
+// }
