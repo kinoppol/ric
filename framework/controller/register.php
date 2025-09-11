406b5d61d3b9218074e1816b1d0e8a4c32e4e50f
kinoppol <kinoppol@hotmail.com>
2025-09-12 04:59:42 +0700

    Update register.php

--- framework/controller/register.php
+++ framework/controller/register.php
@@ -15,9 +15,9 @@ class register{
                 'email'=>$email,
             );
             if($_POSR['role']=='student'){
-                $data['user_type_id']=='4';
+                $data['user_type_id']='4';
             }else if($_POSR['role']=='instructor'){
-                $data['user_type_id']=='3';
+                $data['user_type_id']='3';
             }
         $userModel=model('user_model');
         $user_id=$userModel->add_user($data);
