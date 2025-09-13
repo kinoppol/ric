92d04b98599d3aad6de3ffa00ca2ca35bacef1ac
kinoppol <kinoppol@hotmail.com>
2025-09-13 19:54:44 +0700

    re color

--- framework/view/_template/main.php
+++ framework/view/_template/main.php
@@ -141,24 +141,24 @@ foreach($active_menu as $m){
         <!-- / Menu -->
 
         <!-- Layout container -->
-        <div class="layout-page  content_div" style="background-color:#fdd;">
+        <div class="layout-page  content_div">
           <!-- Navbar -->
 
           <nav
             class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-            id="layout-navbar" style="background-color:#fdd;"
+            id="layout-navbar"
           >
-            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none" style="background-color:#fdd;">
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
               <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
                 <i class="bx bx-menu bx-sm"></i>
               </a>
             </div>
 
-            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse" style="background-color:#fdd;">
+            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
               <!-- Search -->
               <div class="navbar-nav align-items-center">
                 <div class="nav-item d-flex align-items-center">
-                  <h4><b>แพลตฟอร์มการจัดการเรียนการสอนของครูสาขาวิชาการจัดการธุรกิจค้าปลีก</b></h4><!--
+                  <h4 style="color:white;"><b>แพลตฟอร์มการจัดการเรียนการสอนของครูสาขาวิชาการจัดการธุรกิจค้าปลีก</b></h4><!--
                   <i class="bx bx-search fs-4 lh-0"></i>
                   <input
                     type="text"
