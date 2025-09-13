1948915343e1f1172d3237c37ee89e1e8dff9102
kinoppol <kinoppol@hotmail.com>
2025-09-13 17:21:36 +0700

    Update datacenter.php

--- framework/view/ric/datacenter.php
+++ framework/view/ric/datacenter.php
@@ -1,5 +1,111 @@
-<div class="card">
-    <h5 class="card-header">ศูนย์ข้อมูล</h5>
+  <div class="row mb-12 g-6">
+
+<div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">หลักสูตร ปวช. ปี 2567</div>
         <div class="card-body">
+          <h5 class="card-title">สาขาวิชาธุรกิจค้าปลีก</h5>
+          <p class="card-text">ประเภทวิชาบริหารธุรกิจ
+กลุ่มอาชีพการตลาด
+สาขาวิชาธุรกิจค้าปลีก
+</p>
+          <a href="https://bsq.vec.go.th/Portals/9/Course/20/2567/20200/20211v9.pdf" target="_blank" class="btn btn-danger">ดาวน์โหลด</a>
+        </div>
+      </div>
+    </div>
+
+     <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">หลักสูตร ปวส. ปี 2567</div>
+        <div class="card-body">
+          <h5 class="card-title">สาขาวิชาการจัดการธุรกิจค้าปลีก</h5>
+          <p class="card-text">ประเภทวิชาบริหารธุรกิจ
+กลุ่มอาชีพการตลาด
+สาขาวิชาการจัดการธุรกิจค้าปลีก
+</p>
+          <a href="https://bsq.vec.go.th/Portals/9/Course/30/2567/30200/30211v3.pdf" target="_blank" class="btn btn-primary">ดาวน์โหลด</a>
+        </div>
+      </div>
+    </div>
+
+
+    
+     <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">การศึกษาระบบทวิภาคี</div>
+        <div class="card-body">
+          <h5 class="card-title">DVE-DATA</h5>
+          <p class="card-text">ข้อมูลการฝึกงาน<br>
+          ข้อมูลการฝึกอาชีพ
+</p>
+          <a href="https://dve.vec.go.th" target="_blank" class="btn btn-primary">ดูเพิ่มเติม</a>
+        </div>
+      </div>
+    </div>
+
+</div>
+<br>
+<div class="row mb-12 g-6">
+     <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ข้อมูลศูนย์ CVM</div>
+        <div class="card-body">
+          <h5 class="card-title">เกี่ยวกับศูนย์บริหารเครือข่ายฯ (CVM)</h5>
+          <p class="card-text">
+              <a href="https://bsq.vec.go.th/Portals/9/Dowload/your64/Manage/28-6-64/Present_CVM_.pdf" target="_blank">
+1) นำเสนอการประชุมมอบนโยบายการขับเคลื่อนการดำเนินงานศูนย์ CVM</a><br>
+<a href="https://bsq.vec.go.th/Portals/9/Dowload/your64/Manage/28-6-64/manual.pdf" target="_blank">
+2) (ร่าง) คู่มือการขับเคลื่อนการดำเนินงานศูนย์ CVM</a><br>
+<a href="https://bsq.vec.go.th/Portals/9/Dowload/your64/Manage/28-6-64/agenda.pdf" target="_blank">
+3) วาระการประชุมมอบนโยบายการขับเคลื่อนศูนย์ CVM</a><br>
+
+</p>
+      </div>
+    </div>
+        </div>
+
+         <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ตลาดแรงงาน</div>
+        <div class="card-body">
+          <h5 class="card-title">สถานการณ์ตลาดแรงงานรายปีทั่วประเทศ</h5>
+          <p class="card-text">กรมการจัดหางาน<br>
+          วารสารสถานการณ์ตลาดแรงงานรายปี (ทั่วประเทศ)
+</p>
+          <a href="https://catalog.doe.go.th/dataset/dataset13_14" target="_blank" class="btn btn-success">ดูเพิ่มเติม</a>
+        </div>
+      </div>
+    </div>
+
+    
+         <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ข่าวสารค้าปลีก</div>
+        <div class="card-body">
+          <h5 class="card-title">ข่าว : ค้าปลีก</h5>
+          <p class="card-text">ข่าวสารของธุรกิจกลุ่มค้าปลีก<br>
+          จากสำนักข่าวทั่วประเทศ
+</p>
+          <a href="https://www.prachachat.net/tag/ค้าปลีก" target="_blank" class="btn btn-success">ดูเพิ่มเติม</a>
+        </div>
+      </div>
+    </div>
+
+</div>
+<br>
+<div class="row mb-12 g-6">
+     <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ข้อมูลสถานประกอบการ</div>
+        <div class="card-body">
+          <h5 class="card-title">สภานประกอบการกลุ่มค้าปลีก</h5>
+          <p class="card-text">บริษัท ที่ทำธุรกิจด้านค้าปลีก ทั่วประเทศ<br>
+          บริษัทจำกัด มหาชน
+               <a href="https://www.set.or.th/th/market/index/set/consump" target="_blank" class="btn btn-success">รายชื่อบริษัทค้าปลีกชั้นนำ</a>
+        </div>
+
+</p>
+      </div>
+    </div>
         </div>
 </div>
