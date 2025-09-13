871bba3fa1dd1c14a43ace6ce482b913c412ada8
kinoppol <kinoppol@hotmail.com>
2025-09-13 19:02:59 +0700

    media

--- framework/controller/media.php
+++ framework/controller/media.php
@@ -2,8 +2,24 @@
 
 class media{
     function index(){
+        helper('base');
         $data['title']='สื่อการสอน';
-        $data['content']=view('ric/media');
+
+        $courses=model('courses');
+        $user_model=model('user_model');
+        $cond=array('state'=>'ACTIVE');
+        $courses_data=$courses->get_courses($cond);
+
+         $c_data=array();
+        foreach($courses_data as $c){
+            //print_r($c);
+            $owner_data=$user_model->get_user(array('id'=>$c['owner']));
+            $c['owner']=$owner_data[0];
+            $c_data[]=$c;
+        }
+        print_r($c_data);
+        $data['courses']=$c_data;
+        $data['content']=view('ric/media',$data);
         return view('_template/main',$data);
     }
 }
--- framework/view/ric/media.php
+++ framework/view/ric/media.php
@@ -1,5 +1,27 @@
-                <div class="card">
-                    <h5 class="card-header">สื่อการสอน</h5>
-                    <div class="card-body">
-                    </div>
-                </div>
+
+<div class="card mb-4">
+<div class="card-body">
+    <div class="row">
+         <?php
+    foreach($courses as $c){
+ ?>
+<div class="col-md-6 col-xl-4">
+        <div class="card bg-<?php print !empty($c['cover_color'])?$c['cover_color']:'primary'; ?> text-white mb-3">
+            <div class="card-body">
+                <a href="<?php print site_url('media/list/c/'.toBase($c['id'])); ?>">
+                    <h5 class="card-title text-white"><div class="mb-3">
+                    <img src="<?php
+                              print show_avatar($c['owner']['picture']);
+                              ?>" alt
+                        class="w-px-40 h-auto rounded-circle" /> <?php print $c['name']; ?></h5>
+                </a>
+                <p class="card-text"><?php print $c['section']; ?></p>
+            </div>
+        </div>
+        </div>
+    <?php
+    }
+    ?>
+    </div>
+    </div>
+    </div>
