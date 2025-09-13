bb65bcdd39e3066c46afae8d29c1329d8ca9a573
kinoppol <kinoppol@hotmail.com>
2025-09-13 19:09:03 +0700

    Update media.php

--- framework/view/ric/media.php
+++ framework/view/ric/media.php
@@ -9,7 +9,7 @@
         <div class="card bg-<?php print !empty($c['cover_color'])?$c['cover_color']:'primary'; ?> text-white mb-3">
             <div class="card-body">
                 <a href="<?php print site_url('media/list/c/'.toBase($c['id'])); ?>">
-                    <h5 class="card-title text-white"><div class="mb-3">สื่อการสอนรายวิชา<br>
+                    สื่อการสอนรายวิชา<h5 class="card-title text-white"><div class="mb-3">
                     <?php print $c['name']; ?></h5>
                 </a>
                 <p class="card-text">โดย <img src="<?php
