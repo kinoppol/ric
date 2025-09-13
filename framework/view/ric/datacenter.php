9dda7e4f040ff68ca41fc2ebba42ec5cfc39fb66
kinoppol <kinoppol@hotmail.com>
2025-09-13 18:23:41 +0700

    Update datacenter.php

--- framework/view/ric/datacenter.php
+++ framework/view/ric/datacenter.php
@@ -2,12 +2,24 @@
 
 <div class="col-md-6 col-lg-4">
       <div class="card text-left">
-        <div class="card-header">หลักสูตร ปี 2567</div>
+        <div class="card-header">ข้อมูลหลักสูตร</div>
         <div class="card-body">
-          <h5 class="card-title">หลักสูตรระดับ ปวช./ ปวส.</h5>
+          <h5 class="card-title">หลักสูตรระดับ ปวช./ ปวส. 2567</h5>
           <p class="card-text">
         <a href="https://bsq.vec.go.th/Portals/9/Course/20/2567/20200/20211v9.pdf" target="_blank">สาขาวิชาธุรกิจค้าปลีก (ปวช.)</a>
 </p>    <a href="https://bsq.vec.go.th/Portals/9/Course/30/2567/30200/30211v3.pdf" target="_blank">สาขาวิชาการจัดการธุรกิจค้าปลีก (ปวส.)</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ข้อมูลสถานศึกษา</div>
+        <div class="card-body">
+          <h5 class="card-title">สถานศึกษาในสังกัด สอศ.</h5>
+          <p class="card-text">
+        <a href="https://vec.go.th/th-th/หน่วยงานในสังกัด/สถานศึกษาในสังกัดสอศ/สถานศึกษาอาชีวศึกษารัฐบาล.aspx" target="_blank">สถานศึกษาอาชีวศึกษาภาครัฐ</a>
+        <a href="https://vecp.vec.go.th/Portals/68/Doc/School/Private%20Vocational%20School%202567.pdf?ver=2568-06-10-133001-283" target="_blank">สถานศึกษาอาชีวศึกษาภาคเอกชน</a>
         </div>
       </div>
     </div>
@@ -26,6 +38,11 @@
       </div>
     </div>
     
+
+</div>
+<br>
+<div class="row mb-12 g-6">
+
      <div class="col-md-6 col-lg-4">
       <div class="card text-left">
         <div class="card-header">การศึกษาระบบทวิภาคี</div>
@@ -39,10 +56,39 @@
       </div>
     </div>
 
+         <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ข้อมูลตลาดแรงงาน</div>
+        <div class="card-body">
+          <h5 class="card-title">สถานการณ์ตลาดแรงงานรายปีทั่วประเทศ</h5>
+          <p class="card-text">กรมการจัดหางาน<br>
+          วารสารสถานการณ์ตลาดแรงงานรายปี (ทั่วประเทศ)
+</p>
+          <a href="https://catalog.doe.go.th/dataset/dataset13_14" target="_blank" class="btn btn-success">ดูเพิ่มเติม</a>
+        </div>
+      </div>
+    </div>
+
+    
+         <div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ข่าวสารและกิจกรรม</div>
+        <div class="card-body">
+          <h5 class="card-title">ข่าว : ค้าปลีก</h5>
+          <p class="card-text">ข่าวสารของธุรกิจกลุ่มค้าปลีก<br>
+          จากสำนักข่าวทั่วประเทศ
+</p>
+          <a href="https://www.prachachat.net/tag/ค้าปลีก" target="_blank" class="btn btn-success">ดูเพิ่มเติม</a>
+        </div>
+      </div>
+    </div>
+
 </div>
 <br>
+
 <div class="row mb-12 g-6">
-     <div class="col-md-6 col-lg-4">
+
+ <div class="col-md-6 col-lg-4">
       <div class="card text-left">
         <div class="card-header">ข้อมูลศูนย์ CVM</div>
         <div class="card-body">
@@ -59,48 +105,5 @@
       </div>
     </div>
         </div>
-
-         <div class="col-md-6 col-lg-4">
-      <div class="card text-left">
-        <div class="card-header">ตลาดแรงงาน</div>
-        <div class="card-body">
-          <h5 class="card-title">สถานการณ์ตลาดแรงงานรายปีทั่วประเทศ</h5>
-          <p class="card-text">กรมการจัดหางาน<br>
-          วารสารสถานการณ์ตลาดแรงงานรายปี (ทั่วประเทศ)
-</p>
-          <a href="https://catalog.doe.go.th/dataset/dataset13_14" target="_blank" class="btn btn-success">ดูเพิ่มเติม</a>
-        </div>
-      </div>
-    </div>
-
-    
-         <div class="col-md-6 col-lg-4">
-      <div class="card text-left">
-        <div class="card-header">ข่าวสารค้าปลีก</div>
-        <div class="card-body">
-          <h5 class="card-title">ข่าว : ค้าปลีก</h5>
-          <p class="card-text">ข่าวสารของธุรกิจกลุ่มค้าปลีก<br>
-          จากสำนักข่าวทั่วประเทศ
-</p>
-          <a href="https://www.prachachat.net/tag/ค้าปลีก" target="_blank" class="btn btn-success">ดูเพิ่มเติม</a>
-        </div>
-      </div>
-    </div>
-
-</div>
-<br>
-<div class="row mb-12 g-6">
-
-<div class="col-md-6 col-lg-4">
-      <div class="card text-left">
-        <div class="card-header">ข้อมูลสถานศึกษา</div>
-        <div class="card-body">
-          <h5 class="card-title">สถานศึกษาในสังกัด สอศ.</h5>
-          <p class="card-text">
-        <a href="https://vec.go.th/th-th/หน่วยงานในสังกัด/สถานศึกษาในสังกัดสอศ/สถานศึกษาอาชีวศึกษารัฐบาล.aspx" target="_blank">สถานศึกษาอาชีวศึกษาภาครัฐ</a>
-        <a href="https://vecp.vec.go.th/Portals/68/Doc/School/Private%20Vocational%20School%202567.pdf?ver=2568-06-10-133001-283" target="_blank">สถานศึกษาอาชีวศึกษาภาคเอกชน</a>
-        </div>
-      </div>
-    </div>
 
 </div>
