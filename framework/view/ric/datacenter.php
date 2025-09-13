3debd9811d3cb704fb517db8d10ede6a437d8cb4
kinoppol <kinoppol@hotmail.com>
2025-09-13 17:43:41 +0700

    Update datacenter.php

--- framework/view/ric/datacenter.php
+++ framework/view/ric/datacenter.php
@@ -2,33 +2,29 @@
 
 <div class="col-md-6 col-lg-4">
       <div class="card text-left">
-        <div class="card-header">หลักสูตร ปวช. ปี 2567</div>
+        <div class="card-header">หลักสูตร ปี 2567</div>
         <div class="card-body">
-          <h5 class="card-title">สาขาวิชาธุรกิจค้าปลีก</h5>
-          <p class="card-text">ประเภทวิชาบริหารธุรกิจ
-กลุ่มอาชีพการตลาด
-สาขาวิชาธุรกิจค้าปลีก
-</p>
-          <a href="https://bsq.vec.go.th/Portals/9/Course/20/2567/20200/20211v9.pdf" target="_blank" class="btn btn-danger">ดาวน์โหลด</a>
+          <h5 class="card-title">หลักสูตรระดับ ปวช./ ปวส.</h5>
+          <p class="card-text">
+        <a href="https://bsq.vec.go.th/Portals/9/Course/20/2567/20200/20211v9.pdf" target="_blank">สาขาวิชาธุรกิจค้าปลีก (ปวช.)</a>
+</p>    <a href="https://bsq.vec.go.th/Portals/9/Course/30/2567/30200/30211v3.pdf" target="_blank">สาขาวิชาการจัดการธุรกิจค้าปลีก (ปวส.)</a>
         </div>
       </div>
     </div>
 
-     <div class="col-md-6 col-lg-4">
+         <div class="col-md-6 col-lg-4">
       <div class="card text-left">
-        <div class="card-header">หลักสูตร ปวส. ปี 2567</div>
+        <div class="card-header">ข้อมูลสถานประกอบการ</div>
         <div class="card-body">
-          <h5 class="card-title">สาขาวิชาการจัดการธุรกิจค้าปลีก</h5>
-          <p class="card-text">ประเภทวิชาบริหารธุรกิจ
-กลุ่มอาชีพการตลาด
-สาขาวิชาการจัดการธุรกิจค้าปลีก
-</p>
-          <a href="https://bsq.vec.go.th/Portals/9/Course/30/2567/30200/30211v3.pdf" target="_blank" class="btn btn-primary">ดาวน์โหลด</a>
+          <h5 class="card-title">สภานประกอบการกลุ่มค้าปลีก</h5>
+          <p class="card-text">บริษัท ที่ทำธุรกิจด้านค้าปลีก ทั่วประเทศ<br>
+          บริษัทจำกัด มหาชน
+               <a href="https://www.set.or.th/th/market/index/set/consump" target="_blank" class="btn btn-success">รายชื่อบริษัทค้าปลีกชั้นนำ</a>
         </div>
+
+</p>
       </div>
     </div>
-
-
     
      <div class="col-md-6 col-lg-4">
       <div class="card text-left">
@@ -55,10 +51,10 @@
               <a href="https://bsq.vec.go.th/Portals/9/Dowload/your64/Manage/28-6-64/Present_CVM_.pdf" target="_blank">
 1) นำเสนอการประชุมมอบนโยบายการขับเคลื่อนการดำเนินงานศูนย์ CVM</a><br>
 <a href="https://bsq.vec.go.th/Portals/9/Dowload/your64/Manage/28-6-64/manual.pdf" target="_blank">
-2) (ร่าง) คู่มือการขับเคลื่อนการดำเนินงานศูนย์ CVM</a><br>
+2) คู่มือการขับเคลื่อนการดำเนินงานศูนย์ CVM</a><br>
 <a href="https://bsq.vec.go.th/Portals/9/Dowload/your64/Manage/28-6-64/agenda.pdf" target="_blank">
 3) วาระการประชุมมอบนโยบายการขับเคลื่อนศูนย์ CVM</a><br>
-
+<a href="https://cvm.vec.go.th/" target="_blank" class="btn btn-primary">ดูเพิ่มเติม</a>
 </p>
       </div>
     </div>
@@ -94,18 +90,17 @@
 </div>
 <br>
 <div class="row mb-12 g-6">
-     <div class="col-md-6 col-lg-4">
-      <div class="card text-left">
-        <div class="card-header">ข้อมูลสถานประกอบการ</div>
-        <div class="card-body">
-          <h5 class="card-title">สภานประกอบการกลุ่มค้าปลีก</h5>
-          <p class="card-text">บริษัท ที่ทำธุรกิจด้านค้าปลีก ทั่วประเทศ<br>
-          บริษัทจำกัด มหาชน
-               <a href="https://www.set.or.th/th/market/index/set/consump" target="_blank" class="btn btn-success">รายชื่อบริษัทค้าปลีกชั้นนำ</a>
-        </div>
 
-</p>
+<div class="col-md-6 col-lg-4">
+      <div class="card text-left">
+        <div class="card-header">ข้อมูลสถานศึกษา</div>
+        <div class="card-body">
+          <h5 class="card-title">สถานศึกษาในสังกัด สอศ.</h5>
+          <p class="card-text">
+        <a href="https://vec.go.th/th-th/หน่วยงานในสังกัด/สถานศึกษาในสังกัดสอศ/สถานศึกษาอาชีวศึกษารัฐบาล.aspx" target="_blank">สถานศึกษาอาชีวศึกษาภาครัฐ</a>
+        <a href="https://vecp.vec.go.th/Portals/68/Doc/School/Private%20Vocational%20School%202567.pdf?ver=2568-06-10-133001-283" target="_blank">สถานศึกษาอาชีวศึกษาภาคเอกชน</a>
+        </div>
       </div>
     </div>
-        </div>
+
 </div>
