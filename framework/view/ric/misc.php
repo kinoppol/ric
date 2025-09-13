2fa5bec5d5c5736b30faeb52b8def4bd63ee21bb
kinoppol <kinoppol@hotmail.com>
2025-09-13 17:57:21 +0700

    Update misc.php

--- framework/view/ric/misc.php
+++ framework/view/ric/misc.php
@@ -1,5 +1,39 @@
-                <div class="card">
-                    <h5 class="card-header">องค์ประกอบเสริม</h5>
-                    <div class="card-body">
-                    </div>
-                </div>
+<div class="row mb-12 g-6">
+
+<div class="col-md-6 col-lg-4">
+      <div class="card text-center">
+        <div class="card-header">บริการวิชาการและวิชาชีพ</div>
+        <div class="card-body">
+          <h5 class="card-title">โครงการอบรมสัมมนา Upskill Reskill</h5>
+          <p class="card-text">
+</p>
+<a href="https://training.r-hrd.net/Account/Login?ReturnUrl=%2fe-learning%2fEnroll2%2fTrainingWatch2" target="_blank" class="btn btn-primary">รับการอบรม</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="card text-center">
+        <div class="card-header">บทความ</div>
+        <div class="card-body">
+          <h5 class="card-title">สร้างแรงบันดาลใจ บุคคลตัวอย่าง</h5>
+          <p class="card-text">
+</p>
+<a href="https://pnstoretailer.com/top-10-retail-ceos/" target="_blank" class="btn btn-success">อ่านเพิ่มเติม</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 col-lg-4">
+      <div class="card text-center">
+        <div class="card-header">บริการออนไลน์</div>
+        <div class="card-body">
+          <h5 class="card-title">ให้คำปรึกษาออนไลน์</h5>
+          <p class="card-text">
+</p>
+<a href="#"  class="btn btn-warning">นัดหมายรับคำปรึกษา</a>
+        </div>
+      </div>
+    </div>
+
+</div>
