ddc965ab49eabc80cf401885ec1fc40bb2e5c66e
kinoppol <kinoppol@hotmail.com>
2025-09-13 19:51:25 +0700

    re css

--- framework/view/_template/main.php
+++ framework/view/_template/main.php
@@ -141,20 +141,20 @@ foreach($active_menu as $m){
         <!-- / Menu -->
 
         <!-- Layout container -->
-        <div class="layout-page  content_div">
+        <div class="layout-page  content_div" style="background-color:#fdd;">
           <!-- Navbar -->
 
           <nav
             class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-            id="layout-navbar"
+            id="layout-navbar" style="background-color:#fdd;"
           >
-            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none" style="background-color:#fdd;">
               <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
                 <i class="bx bx-menu bx-sm"></i>
               </a>
             </div>
 
-            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse" style="background-color:#fdd;">
               <!-- Search -->
               <div class="navbar-nav align-items-center">
                 <div class="nav-item d-flex align-items-center">
